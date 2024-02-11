00d8900bc51b64d71dbdcf443bc473ea09be76cc
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-02-11 16:42:33 +0200

    added Shoulder TestPage5

--- lib/view/shoulder/widgets/shoulder_test_page5.dart
+++ lib/view/shoulder/widgets/shoulder_test_page5.dart
@@ -5,10 +5,39 @@ class _ShoulderTestPage5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppText(title: "_ShoulderTestPage5"),
-      ],
+    return BlocBuilder<ShoulderCubit, ShoulderStates>(
+      builder: (context, state) {
+        final cubit = ShoulderCubit.of(context);
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _spacer(),
+            AppToggle(
+              optionNames: const ['+ve', '-ve', 'not done'],
+              label: "O’Brien test",
+            ),
+            _spacer(),
+            AppToggle(
+              optionNames: const ['+ve', '-ve', 'not done'],
+              label: "Passive distraction test",
+            ),
+            _spacer(),
+            AppToggle(
+              optionNames: const ['+ve', '-ve', 'not done'],
+              label: "Modified labral shear test",
+            ),
+            _spacer(),
+            AppTextField(
+              hint: "note",
+            ),
+            _spacer(),
+            AppTextField(
+              hint: "note",
+              label: "Treatments",
+            ),
+          ],
+        );
+      },
     );
   }
 }
