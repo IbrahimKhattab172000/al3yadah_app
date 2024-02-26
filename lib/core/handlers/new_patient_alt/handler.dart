fcfbce71ff762b3fffa899a6def45997f666eb81
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-02-27 00:45:00 +0200

    updated NewPatientAltHandler

--- lib/core/handlers/new_patient_alt/handler.dart
+++ lib/core/handlers/new_patient_alt/handler.dart
@@ -1,12 +1,17 @@
 import 'package:al3yadah_app/core/models/patient_general.dart';
 import 'package:al3yadah_app/core/models/shoulder.dart';
+import 'package:al3yadah_app/core/repository/new_patient.dart';
+import 'package:al3yadah_app/core/route_utils/route_utils.dart';
+import 'package:al3yadah_app/view/success/view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'state.dart';
 
 class NewPatientAltHandler extends Cubit<NewPatientAltState> {
-  NewPatientAltHandler() : super(NewPatientAltInitial()) {
+  final NewPatientRepository newPatientRepository;
+  NewPatientAltHandler({required this.newPatientRepository})
+      : super(NewPatientAltInitial()) {
     _initial();
   }
 
@@ -47,20 +52,38 @@ class NewPatientAltHandler extends Cubit<NewPatientAltState> {
   ];
   // String? presentedArea;
 
-  final int totalSteps = 5;
-  int currentStep = 1;
-
-  bool get isLast => currentStep == 5;
-  bool get isFirst => currentStep == 1;
-
-  void nextStep() {
-    currentStep++;
-    emit((state as NewPatientAltShoulder));
+  dynamic selectedIndexInterpretation({required int selectedIndex}) {
+    if (selectedIndex == 0) {
+      return "+ve";
+    } else if (selectedIndex == 1) {
+      return "-ve";
+    } else {
+      return "not done";
+    }
   }
 
-  void previousStep() {
-    currentStep--;
-    emit((state as NewPatientAltShoulder));
+  void nextStepShoulder() {
+    if ((state as NewPatientAltShoulder).currentStep < 5) {
+      emit((state as NewPatientAltShoulder).copyWith(
+          currentStep: (state as NewPatientAltShoulder).currentStep + 1));
+      print((state as NewPatientAltShoulder).currentStep);
+    } else {
+      submitNewPatient();
+      RouteUtils.navigateTo(SuccessView());
+    }
+  }
+
+  void previousStepShoulder() {
+    if (!(state as NewPatientAltShoulder).isFirst) {
+      emit((state as NewPatientAltShoulder).copyWith(
+          currentStep: (state as NewPatientAltShoulder).currentStep - 1));
+      print((state as NewPatientAltShoulder).currentStep);
+    } else {
+      print((state as NewPatientAltShoulder).currentStep);
+
+      emit(NewPatientAltGeneralInfo(
+          patientGeneral: (state as NewPatientAltShoulder).patientGeneral));
+    }
   }
 
   updateGeneralValues({
@@ -95,6 +118,7 @@ class NewPatientAltHandler extends Cubit<NewPatientAltState> {
       emit(
         NewPatientAltShoulder(
           patientGeneral: (state as NewPatientAltGeneralInfo).patientGeneral,
+          currentStep: 1,
         ),
       );
     } else if (state is NewPatientAltGeneralInfo &&
@@ -102,26 +126,103 @@ class NewPatientAltHandler extends Cubit<NewPatientAltState> {
             "Knee") {
       emit(NewPatientAltKnee());
     } else {
-      emit(NewPatientAltAnkle());
+      return;
     }
   }
 
-  moveInThePresentedArea({required String presentedArea}) {
-    if (state is NewPatientAltGeneralInfo &&
-        (state as NewPatientAltGeneralInfo).patientGeneral.presentedArea ==
-            "Shoulder") {
-      emit(
-        NewPatientAltShoulder(
-          patientGeneral: (state as NewPatientAltGeneralInfo).patientGeneral,
-          shoulder: null,
+  updateShoulderValues({
+    bool? cervicalFree,
+    String? history,
+    String? suspectDiagnose,
+    String? place,
+    String? vas,
+    String? palpation,
+    String? flexionNote,
+    int? flexionNum,
+    String? extensionNote,
+    int? extensionNum,
+    String? abdNote,
+    int? abdNum,
+    String? addNote,
+    int? addNum,
+    String? erNote,
+    int? erNum,
+    String? irNote,
+    int? irNum,
+    String? hawkinsTest,
+    String? neerTest,
+    String? posterior,
+    String? impingementNote,
+    String? apprehension,
+    String? relocation,
+    String? instabilityNote,
+    String? bellyOff,
+    String? irls,
+    String? erls,
+    String? rcTearNote,
+    String? scapularAssistanceTest,
+    String? scapularRetractionTest,
+    String? scapularStabilityNote,
+    String? acromioTest,
+    String? obrienTest,
+    String? passiveTest,
+    String? modifiedTest,
+    String? obrienTestNote,
+    String? treatmentNote,
+  }) {
+    final currentShoulder =
+        (state as NewPatientAltShoulder).shoulder ?? Shoulder();
+
+    emit(
+      (state as NewPatientAltShoulder).copyWith(
+        shoulder: Shoulder(
+          cervicalFree: cervicalFree ?? currentShoulder.cervicalFree,
+          history: history ?? currentShoulder.history,
+          suspectDiagnose: suspectDiagnose ?? currentShoulder.suspectDiagnose,
+          place: place ?? currentShoulder.place,
+          vas: vas ?? currentShoulder.vas,
+          palpation: palpation ?? currentShoulder.palpation,
+          flexionNote: flexionNote ?? currentShoulder.flexionNote,
+          flexionNum: flexionNum ?? currentShoulder.flexionNum,
+          extensionNote: extensionNote ?? currentShoulder.extensionNote,
+          extensionNum: extensionNum ?? currentShoulder.extensionNum,
+          abdNote: abdNote ?? currentShoulder.abdNote,
+          abdNum: abdNum ?? currentShoulder.abdNum,
+          addNote: addNote ?? currentShoulder.addNote,
+          addNum: addNum ?? currentShoulder.addNum,
+          erNote: erNote ?? currentShoulder.erNote,
+          erNum: erNum ?? currentShoulder.erNum,
+          irNote: irNote ?? currentShoulder.irNote,
+          irNum: irNum ?? currentShoulder.irNum,
+          hawkinsTest: hawkinsTest ?? currentShoulder.hawkinsTest,
+          neerTest: neerTest ?? currentShoulder.neerTest,
+          posterior: posterior ?? currentShoulder.posterior,
+          impingementNote: impingementNote ?? currentShoulder.impingementNote,
+          apprehension: apprehension ?? currentShoulder.apprehension,
+          relocation: relocation ?? currentShoulder.relocation,
+          instabilityNote: instabilityNote ?? currentShoulder.instabilityNote,
+          bellyOff: bellyOff ?? currentShoulder.bellyOff,
+          irls: irls ?? currentShoulder.irls,
+          erls: erls ?? currentShoulder.erls,
+          rcTearNote: rcTearNote ?? currentShoulder.rcTearNote,
+          scapularAssistanceTest:
+              scapularAssistanceTest ?? currentShoulder.scapularAssistanceTest,
+          scapularRetractionTest:
+              scapularRetractionTest ?? currentShoulder.scapularRetractionTest,
+          acromioTest: acromioTest ?? currentShoulder.acromioTest,
+          obrienTest: obrienTest ?? currentShoulder.obrienTest,
+          passiveTest: passiveTest ?? currentShoulder.passiveTest,
+          modifiedTest: modifiedTest ?? currentShoulder.modifiedTest,
+          obrienTestNote: obrienTestNote ?? currentShoulder.obrienTestNote,
+          scapularStabilityNote:
+              scapularStabilityNote ?? currentShoulder.scapularStabilityNote,
+          treatmentNote: treatmentNote ?? currentShoulder.treatmentNote,
         ),
-      );
-    } else if (state is NewPatientAltGeneralInfo &&
-        (state as NewPatientAltGeneralInfo).patientGeneral.presentedArea ==
-            "Knee") {
-      emit(NewPatientAltKnee());
-    } else {
-      emit(NewPatientAltAnkle());
-    }
+      ),
+    );
+  }
+
+  submitNewPatient() async {
+    // newPatientRepository.addPatient(patient: Patient);
   }
 }
