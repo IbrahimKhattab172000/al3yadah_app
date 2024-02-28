844b0e2e6e8644fe739614eae17eee0e8ea4eafb
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-02-28 17:07:41 +0200

    added getPatients to NewPatientRepository

--- lib/core/repository/new_patient.dart
+++ lib/core/repository/new_patient.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:al3yadah_app/core/models/patient.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NewPatientRepository {
   final CollectionReference _patientsCollection =
@@ -10,6 +10,23 @@ class NewPatientRepository {
       await _patientsCollection.add(patient.toJson());
     } catch (e) {
       print('Error adding patient: $e');
+      rethrow;
+    }
+  }
+
+  Future<List<Patient>> getPatients() async {
+    try {
+      QuerySnapshot querySnapshot = await _patientsCollection.get();
+      List<Patient> patients = [];
+
+      for (QueryDocumentSnapshot documentSnapshot in querySnapshot.docs) {
+        patients.add(
+            Patient.fromJson(documentSnapshot.data() as Map<String, dynamic>));
+      }
+
+      return patients;
+    } catch (e) {
+      print('Error getting patients: $e');
       rethrow;
     }
   }
