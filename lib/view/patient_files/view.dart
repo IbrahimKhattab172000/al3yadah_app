f11630d1719f44226e7b27962cfff095fc75a12b
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-14 20:32:09 +0200

    adjusted PatientFilesView

--- lib/view/patient_files/view.dart
+++ lib/view/patient_files/view.dart
@@ -1,11 +1,13 @@
 import 'package:al3yadah_app/core/handlers/new_patient_alt/handler.dart';
+import 'package:al3yadah_app/core/helpers/app_colors.dart';
+import 'package:al3yadah_app/core/helpers/dimensions.dart';
 import 'package:al3yadah_app/core/helpers/utils.dart';
 import 'package:al3yadah_app/core/models/patient.dart';
-import 'package:al3yadah_app/core/repository/patient_main.dart';
 import 'package:al3yadah_app/view/add_session/view.dart';
 import 'package:al3yadah_app/view/patient_files/widget/session_card.dart';
+import 'package:al3yadah_app/widgets/app/app_data_show_card.dart';
 import 'package:al3yadah_app/widgets/app_app_bar.dart';
-
+import 'package:al3yadah_app/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,19 +16,114 @@ class PatientFilesView extends StatelessWidget {
 
   const PatientFilesView({Key? key, required this.patient}) : super(key: key);
 
+  Widget _buildCard(String title, dynamic value) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppText(
+              title: title,
+              color: AppColors.primary,
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+            const SizedBox(height: 5),
+            AppText(
+              title: value.toString(),
+              color: AppColors.txtFieldlable2,
+              fontSize: 14,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PatientMainHandler, PatientMainState>(
       builder: (context, state) {
         return Scaffold(
           appBar: AppAppBar(title: patient.name),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    physics: const BouncingScrollPhysics(),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ...[
+                    AppDataShowCard(
+                      title: "Name",
+                      value: patient.name,
+                    ),
+                    AppDataShowCard(
+                      title: "Age",
+                      value: patient.age.toString(),
+                    ),
+                    AppDataShowCard(
+                      title: "Occupation",
+                      value: patient.occupation,
+                    ),
+                    AppDataShowCard(
+                      title: "Medical Reference",
+                      value: patient.medicalRef,
+                    ),
+                    AppDataShowCard(
+                      title: "Weight",
+                      value: patient.weight.toString(),
+                    ),
+                    AppDataShowCard(
+                      title: "Chief Complaint",
+                      value: patient.chiefComplaint,
+                    ),
+                    AppDataShowCard(
+                      title: "Course",
+                      value: patient.course,
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AppText(
+                          title: "Last Presented Area:",
+                          color: AppColors.txtFieldlable2,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        SizedBox(width: 20.width),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: AppText(
+                            title: patient.presentedArea,
+                            color: AppColors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                  SizedBox(height: 24.height),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
                     itemCount: patient.sessions.length,
                     itemBuilder: (context, index) {
                       return SessionCard(
@@ -35,13 +132,14 @@ class PatientFilesView extends StatelessWidget {
                       );
                     },
                   ),
-                ),
-                AddSessionView(
-                  newPatientAltHandler: context.read<PatientMainHandler>(),
-                  currentPatient: patient,
-                ),
-                SizedBox(height: Utils.bottomDevicePadding),
-              ],
+                  SizedBox(height: 24.height),
+                  AddSessionView(
+                    newPatientAltHandler: context.read<PatientMainHandler>(),
+                    currentPatient: patient,
+                  ),
+                  SizedBox(height: Utils.bottomDevicePadding),
+                ],
+              ),
             ),
           ),
         );
