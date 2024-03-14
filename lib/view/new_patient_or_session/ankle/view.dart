d12dea04ca0636f92fbeeb4ffa0ccb3475c93db6
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-14 22:16:41 +0200

    added AnkleContentView

---
+++ lib/view/new_patient_or_session/ankle/view.dart
@@ -0,0 +1,31 @@
+import 'package:al3yadah_app/core/handlers/new_patient_alt/handler.dart';
+import 'package:al3yadah_app/core/helpers/app_colors.dart';
+import 'package:al3yadah_app/core/helpers/dimensions.dart';
+import 'package:al3yadah_app/core/helpers/validator.dart';
+import 'package:al3yadah_app/widgets/app/app_double_form_field.dart';
+import 'package:al3yadah_app/widgets/app/app_toggle.dart';
+import 'package:al3yadah_app/widgets/app_text.dart';
+import 'package:al3yadah_app/widgets/app_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'widgets/ankle_test_page1.dart';
+part 'widgets/ankle_test_page2.dart';
+
+class AnkleContentView extends StatelessWidget {
+  final PatientMainStateAnkle state;
+  const AnkleContentView({Key? key, required this.state}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state.currentStep) {
+      case 1:
+        return _AnkleTestPage1();
+      case 2:
+        return _AnkleTestPage2();
+
+      default:
+        return SizedBox(child: AppText(title: "none"));
+    }
+  }
+}
