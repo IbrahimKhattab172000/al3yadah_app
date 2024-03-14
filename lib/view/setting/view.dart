7e2c41c49f44427350afa09f4fc46fdee6983d3b
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-14 20:27:33 +0200

    updated SettingView

--- lib/view/setting/view.dart
+++ lib/view/setting/view.dart
@@ -1,3 +1,6 @@
+import 'package:al3yadah_app/core/helpers/app_colors.dart';
+import 'package:al3yadah_app/core/helpers/dimensions.dart';
+import 'package:al3yadah_app/core/helpers/utils.dart';
 import 'package:al3yadah_app/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +10,25 @@ class SettingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Center(child: AppText(title: "Settings")),
-      ]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              Utils.getAssetPNGPath("future_work"),
+              width: 300.width,
+              height: 150.height,
+            ),
+          ),
+          SizedBox(height: 24.height),
+          AppText(
+            title: "Future Work",
+            color: const Color(0xff2D6165),
+            fontSize: 30,
+            fontWeight: FontWeight.w900,
+          ),
+        ],
+      ),
     );
   }
 }
