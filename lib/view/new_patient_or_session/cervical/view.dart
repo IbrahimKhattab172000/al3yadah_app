42464b9e544adb1942ce55834ee9c8aa2b28f650
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-23 16:12:30 +0200

    added CervicalContentView

---
+++ lib/view/new_patient_or_session/cervical/view.dart
@@ -0,0 +1,36 @@
+import 'package:al3yadah_app/core/handlers/new_patient_alt/handler.dart';
+import 'package:al3yadah_app/core/helpers/app_colors.dart';
+import 'package:al3yadah_app/core/helpers/dimensions.dart';
+import 'package:al3yadah_app/core/helpers/validator.dart';
+import 'package:al3yadah_app/widgets/app/app_double_form_field.dart';
+import 'package:al3yadah_app/widgets/app/app_toggle.dart';
+import 'package:al3yadah_app/widgets/app_text.dart';
+import 'package:al3yadah_app/widgets/app_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'widgets/cervical_test_page1.dart';
+part 'widgets/cervical_test_page2.dart';
+part 'widgets/cervical_test_page3.dart';
+part 'widgets/cervical_test_page4.dart';
+
+class CervicalContentView extends StatelessWidget {
+  final PatientMainStateCervical state;
+  const CervicalContentView({super.key, required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state.currentStep) {
+      case 1:
+        return _CervicalTestPage1();
+      case 2:
+        return __CervicalTestPage2();
+      case 3:
+        return __CervicalTestPage3();
+      case 4:
+        return __CervicalTestPage4();
+      default:
+        return SizedBox(child: AppText(title: "none"));
+    }
+  }
+}
